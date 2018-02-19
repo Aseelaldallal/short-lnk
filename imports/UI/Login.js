a59a5971d161001771d5d90f0c17d7ba61bb140f
unknown <aseelaldallal@gmail.com>
2018-02-19 11:44:46 -0500

    implement login, signup, logout

--- imports/UI/Login.js
+++ imports/UI/Login.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+    Meteor.loginWithPassword({ email }, password, err => {
+      console.log('Login Callback', err);
+    });
+  };
+
   render() {
     console.log(this.props);
     return (
       <div>
-        <h1>Login to Short Lnk </h1>login form here
+        <h1> Short Link </h1>
+        {this.state.error ? <p> {this.state.error} </p> : null}
+        <form onSubmit={this.onSubmit}>
+          <input type="email" ref="email" name="email" placeholder="Email" />
+          <input
+            type="password"
+            ref="password"
+            name="password"
+            placeholder="Password"
+          />
+          <button> Login </button>
+        </form>
         <Link to="/signup"> Don't have an account? </Link>
       </div>
     );
